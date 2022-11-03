f64551d6e66dc8d887cc380df4e132d389c8326e
834772509 <834772509@qq.com>
2022-11-03 16:46:57 +0800

    feat: add auto clear token data

--- src/stores/modules/main.ts
+++ src/stores/modules/main.ts
@@ -6,7 +6,9 @@ import router from "@/router";
 export const useMainStore = defineStore("main", {
   state: () => ({
     token: "",
+    // 用户信息
     userInfo: {} as any,
+    // 是否显示加载框
     showLoading: false,
   }),
   actions: {
@@ -14,9 +16,11 @@ export const useMainStore = defineStore("main", {
       // 1.实现登录逻辑
       const loginResult = await accountLoginRequest(username, password);
 
-      const { id, token } = loginResult.data;
+      const { id, token, expiresAt } = loginResult.data;
       this.token = token;
+
       localCache.setCache("token", token);
+      localCache.setCache("expiresAt", expiresAt);
 
       // 2.请求用户信息
       const userInfoResult = await requestUserInfoById(id);
@@ -27,6 +31,12 @@ export const useMainStore = defineStore("main", {
       router.push("/");
     },
     loadLocalLogin() {
+      const expiresAt = localCache.getCache("expiresAt");
+      if (expiresAt && new Date().getTime() > new Date(expiresAt).getTime()) {
+        // token 已过期，清除本地 token
+        localCache.clearCacke();
+      }
+
       const token = localCache.getCache("token");
       if (token) {
         this.token = token;
