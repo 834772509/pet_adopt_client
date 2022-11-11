f897b6247748cb5113345cb926d7c3f6e64b09cc
834772509 <834772509@qq.com>
2022-11-11 16:19:27 +0800

    feat: add contact page content

--- src/views/mine/views/contact/contact.vue
+++ src/views/mine/views/contact/contact.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="contact"></div>
+  <div class="contact">
+    <van-cell-group inset>
+      <van-cell title="QQ群" value="0000000000" />
+      <van-cell title="微信" value="xxxxxx" />
+      <van-cell title="公众号" value="xxxxxx" />
+      <van-cell title="微博" value="xxxxxx" />
+      <van-cell title="邮箱" value="xxxxxx@xxx.com" />
+      <van-cell title="电话" value="000 0000 0000" />
+      <van-cell title="官网" value="www.xxx.com" />
+    </van-cell-group>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.contact {
+  margin-top: 10px;
+}
+</style>
