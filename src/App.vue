7ffa38477a8ad1ea0008f68726acdd0d90d54ce8
834772509 <834772509@qq.com>
2022-11-30 17:07:10 +0800

    feat: add dynamic keepAlive

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,11 @@
 
     <!-- App内容 -->
     <router-view v-slot="{ Component }">
-      <transition :enter-active-class="$route.meta.transition">
-        <keep-alive :exclude="['detail', 'stars']">
-          <component :is="Component" /> </keep-alive
-      ></transition>
+      <keep-alive :exclude="excludeList">
+        <transition :enter-active-class="$route.meta.transition">
+          <component :is="Component" />
+        </transition>
+      </keep-alive>
     </router-view>
 
     <!-- 底部导航栏 -->
@@ -23,16 +24,32 @@
 </template>
 
 <script lang="ts" setup>
+import { ref, watch } from "vue";
+import { useRoute } from "vue-router";
 import NavBar from "@/components/nav-bar/nav-bar.vue";
 import TabBar from "@/components/tab-bar/tab-bar.vue";
 import Loading from "@/components/loading/loading.vue";
-import backTop from "@/components/backtop/backtop.vue";
+import BackTop from "@/components/backtop/backtop.vue";
+
+// 缓存页面排除列表
+const excludeList = ref([] as any[]);
+
+const route = useRoute();
+watch(
+  () => route.meta.keepAlive,
+  (keepAlive) => {
+    if (keepAlive === false) {
+      !excludeList.value.includes(route.name) &&
+        excludeList.value.push(route.name);
+    }
+  }
+);
 </script>
 
 <style lang="less" scoped>
 .app {
   --animate-duration: 0.5s;
   height: 100%;
-  background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
+  // background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
 }
 </style>
